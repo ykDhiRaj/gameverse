3c2cb4a5d948df909503fbe9529bb931f0d92dcc
Dhiraj <feelsnmess@gmail.com>
2025-02-18 15:39:02 +0530

    sidebar changes

--- frontend/src/components/SideBarComponent.jsx
+++ frontend/src/components/SideBarComponent.jsx
@@ -5,22 +5,29 @@ import {
   IoHeart,
   IoSettings,
   IoBookmark,
+  IoLogIn
 } from "react-icons/io5";
 import { FaUser } from 'react-icons/fa6';
 import { useNavigate, useLocation } from "react-router-dom";
+import {useSelector} from "react-redux"
+
 
 function SideBarComponent() {
   const navigate = useNavigate();
   const location = useLocation();
+  const user = useSelector((state)=>state.user.user);
 
   const menuItems = [
     { icon: IoHomeSharp, label: "Home", path: "/" },
     { icon: IoCalendar, label: "New Releases", path: "/new-releases" },
     { icon: IoHeart, label: "Favorites", path: "/favorites" },
     { icon: IoBookmark, label: "Wishlist", path: "/wishlist" },
-    { icon: FaUser, label: "Profile", path: "/profile" },
+    user ? 
+    { icon: FaUser, label: "Profile", path: "/profile" } : null,
     { icon: IoSettings, label: "Settings", path: "/settings" },
-  ];
+    !user ? 
+      { icon: IoLogIn, label: "Login", path: "/login" } : null
+  ].filter(Boolean);
 
   const handleNavigation = (path) => {
     if (path === "/" && location.pathname === "/") {
@@ -36,7 +43,7 @@ function SideBarComponent() {
       <div className="bg-[#202020] text-gray-100 px-4 py-6 flex flex-col transition-all duration-300 ease-in-out rounded-lg">
         <nav className="flex-1">
           <ul className="space-y-2">
-            {menuItems.map((item, index) => (
+            {menuItems.map((item, index)=>(
               <li key={index}>
                 <button
                   onClick={() => handleNavigation(item.path)}
